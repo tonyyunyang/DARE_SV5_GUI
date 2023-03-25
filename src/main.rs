3f71b5bb0f7d44356b9b58b7259c6823ededaa8d
Tony <tonyyunyang@outlook.com>
2023-03-25 22:45:34 +0100

    small fmt & clippy

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use dare_sv5::TemplateApp;
-
-
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
