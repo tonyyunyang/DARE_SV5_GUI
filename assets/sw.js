52955d12137635677fb45739b93ab67122af69cc
Tony <tonyyunyang@outlook.com>
2023-03-25 22:27:48 +0100

    A good start.

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './dare_sv5.js',
+  './dare_sv5_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
